4765e594dd14c82b21617f9c58dd926797d2acbd
mualal <almuravcev@yandex.ru>
2023-11-10 22:47:41 +0300

    set axis limits and other minor improvements

--- matlab/lattice_lattice_interface.m
+++ matlab/lattice_lattice_interface.m
@@ -22,7 +22,8 @@ gamma = pi/4.5;
 %k_1 = asin(omega/2 * sqrt(m_1/c)) * 2 / a;
 
 syms k;
-k_1 = double(vpasolve(m_1*omega^2==4*c*(sin(cos(gamma)*k)^2+sin(sin(gamma)*k)^2), k, 1))*2/a;
+k_1 = double(vpasolve(m_1*omega^2==4*c*(sin(cos(gamma)*k)^2+...
+    sin(sin(gamma)*k)^2), k, 1))*2/a;
 
 num_x = round((left_x:a:right_x)/a);
 num_y = round((left_y:a:right_y)/a);
@@ -67,24 +68,38 @@ vel = zeros(length(num_y),length(num_x));
 
 beta_x = 0.1;
 beta_y = 0.1;
-n_0 = -30;
-v_drift = 25;
+n_0 = -35;
+v_0 = -35;
 u_0 = 1;
 %g_1 = a * sqrt(c/m_1) * cos(k_1*a/2);
-g_1 = sqrt(c/m_1)*(a*cos(gamma)*cos(cos(gamma)*k_1*a/2)*sin(cos(gamma)*k_1*a/2)-a*sin(gamma)*cos(sin(gamma)*k_1*a/2)*sin(sin(gamma)*k_1*a/2))/(sqrt((cos(sin(gamma)*k_1*a/2))^2+(sin(cos(gamma)*k_1*a/2))^2));
+g_1 = sqrt(c/m_1)*(a*cos(gamma)*cos(cos(gamma)*k_1*a/2)*...
+    sin(cos(gamma)*k_1*a/2)-a*sin(gamma)*cos(sin(gamma)*k_1*a/2)*...
+    sin(sin(gamma)*k_1*a/2))/(sqrt((cos(sin(gamma)*k_1*a/2))^2+...
+    (sin(cos(gamma)*k_1*a/2))^2));
 
 % initial conditions
 for i=1:length(num_x)
     for j=1:length(num_y)
-        %disp(j, i) = u_0 * exp(-beta_x^2/2 * (num_x(i) - n_0)^2) * exp(-beta_y^2/2 * (num_y(j))^2);
+        %disp(j, i) = u_0 * exp(-beta_x^2/2 * (num_x(i) - n_0)^2) * ...
+        %    exp(-beta_y^2/2 * (num_y(j))^2);
         %disp(j, i) = disp(j, i) * sin(num_x(i) * k_1 * a);
-        disp(j, i) = u_0 * exp(-beta_x^2/2 * (num_x(i)*cos(gamma)+num_y(j)*sin(gamma)-n_0*cos(gamma)+v_drift)^2);
-        disp(j, i) = disp(j, i) * exp(-beta_y^2/2 * (-num_x(i)*sin(gamma)+num_y(j)*cos(gamma)+n_0*sin(gamma)+v_drift)^2);
-        disp(j, i) = disp(j, i) * sin(k_1*a*cos(gamma)*num_x(i)+k_1*a*sin(gamma)*num_y(j));
-        vel(j, i) = -u_0 * exp(-beta_x^2/2 * (num_x(i)*cos(gamma)+num_y(j)*sin(gamma)-n_0*cos(gamma)+v_drift)^2);
-        vel(j, i) = vel(j, i) * exp(-beta_y^2/2 * (-num_x(i)*sin(gamma)+num_y(j)*cos(gamma)+n_0*sin(gamma)+v_drift)^2);
-        vel(j, i) = vel(j, i) * (omega*cos(k_1*a*cos(gamma)*num_x(i)+k_1*a*sin(gamma)*num_y(j)) - beta_x^2*g_1/a*(num_x(i)*cos(gamma)+num_y(j)*sin(gamma)-n_0*cos(gamma)+v_drift)*sin(k_1*a*cos(gamma)*num_x(i)+k_1*a*sin(gamma)*num_y(j)));
-        %vel(j, i) = vel(j, i) * (omega * cos(k_1*(num_x(i))) - beta_x^2*g_1/a*(num_x(i)-n_0)*sin(num_x(i) * k_1 * a));
+        disp(j, i) = u_0 * exp(-beta_x^2/2 * (num_x(i)*cos(gamma)+...
+            num_y(j)*sin(gamma)-n_0*cos(gamma)-v_0*sin(gamma))^2);
+        disp(j, i) = disp(j, i) * exp(-beta_y^2/2 * (-num_x(i)*...
+            sin(gamma)+num_y(j)*cos(gamma)+n_0*sin(gamma)-...
+            v_0*cos(gamma))^2);
+        disp(j, i) = disp(j, i) * sin(k_1*a*cos(gamma)*num_x(i)+...
+            k_1*a*sin(gamma)*num_y(j));
+        vel(j, i) = -u_0 * exp(-beta_x^2/2 * (num_x(i)*cos(gamma)+...
+            num_y(j)*sin(gamma)-n_0*cos(gamma)-v_0*sin(gamma))^2);
+        vel(j, i) = vel(j, i) * exp(-beta_y^2/2 * (-num_x(i)*sin(gamma)...
+            +num_y(j)*cos(gamma)+n_0*sin(gamma)-v_0*cos(gamma))^2);
+        vel(j, i) = vel(j, i) * (omega*cos(k_1*a*cos(gamma)*num_x(i)+...
+            k_1*a*sin(gamma)*num_y(j)) - beta_x^2*g_1/a*(num_x(i)*...
+            cos(gamma)+num_y(j)*sin(gamma)-n_0*cos(gamma))*...
+            sin(k_1*a*cos(gamma)*num_x(i)+k_1*a*sin(gamma)*num_y(j)));
+        %vel(j, i) = vel(j, i) * (omega * cos(k_1*(num_x(i))) - ...
+        %    beta_x^2*g_1/a*(num_x(i)-n_0)*sin(num_x(i) * k_1 * a));
     end
 end
 
@@ -100,8 +115,9 @@ result = cell(1, fix(t_max/save_time));
 % equations of motion integration
 for t=times
     %disp(int32(length(num_y)/a), int32(length(num_x)/a)) = sin(omega*t);
-    %vel(int32(length(num_y)/a), int32(length(num_x)/a)) = omega*cos(omega*t);
-    vel = vel + c./mass.*(circshift(disp,[-1 0])+circshift(disp,[1 0])+circshift(disp,[0 -1])+circshift(disp,[0 1])-4*disp).*dt;
+    %vel(int32(length(num_y)/a),int32(length(num_x)/a))=omega*cos(omega*t);
+    vel = vel + c./mass.*(circshift(disp,[-1 0])+circshift(disp,[1 0])+...
+        circshift(disp,[0 -1])+circshift(disp,[0 1])-4*disp).*dt;
     disp = disp + vel.*dt;
     if rem(t, save_time) == 0
         result{fix(t/save_time)+1} = disp;
@@ -113,11 +129,13 @@ f1 = figure(1); hold on
 f1.Position = [50,50,750,650];
 [X,Y] = meshgrid(num_x,num_y);
 surf(X,Y,disp_initial,'FaceAlpha',0.9,'EdgeAlpha',0.5);
-plot3(x_coord,y_coord,max(max(disp_initial))*ones(1,length(x_coord)),'LineWidth',2,'Color','Black')
+plot3(x_coord,y_coord,max(max(disp_initial))*ones(1,length(x_coord)),...
+    'LineWidth',2,'Color','Black')
 title('Цветовая карта перемещений в момент времени t = 0');
 xlabel('Номер частицы по оси Ox');
 ylabel('Номер частицы по оси Oy');
 colorbar;
+axis([num_x(1) num_x(end) num_y(1) num_y(end)])
 pbaspect([1,(right_y-left_y)/(right_x-left_x),1]);
 view(17,22);
 hold off
@@ -129,18 +147,21 @@ s1 = surf(X,Y,disp,'FaceAlpha',0.9,'EdgeAlpha',0.7);
 title('Цветовая карта перемещений в момент времени t = '+string(t_max));
 xlabel('Номер частицы по оси Ox');
 ylabel('Номер частицы по оси Oy');
-l1 = plot3(x_coord,y_coord,10*ones(1,length(x_coord)),'LineWidth',2,'Color','Black');
+l1 = plot3(x_coord,y_coord,10*ones(1,length(x_coord)),'LineWidth',2,...
+    'Color','Black');
 colorbar;
 caxis([-1 1])
 %s1.EdgeColor = 'none';
 %view(17,22);
+axis([num_x(1) num_x(end) num_y(1) num_y(end)])
 pbaspect([1,(right_y-left_y)/(right_x-left_x),1]);
 for i = 1:length(result)
     s1.XData = X;
     s1.YData = Y;
     s1.ZData = result{i};
     l1.ZData = max(max(result{i}))*ones(1,length(x_coord));
-    title('Цветовая карта перемещений в момент времени t = '+string((i-1)*save_time));
+    title('Цветовая карта перемещений в момент времени t = '+...
+        string((i-1)*save_time));
     if write_video
         filename = 'new-test-1.gif';
         frame = getframe(2); 
@@ -149,7 +170,8 @@ for i = 1:length(result)
         if i == 1
            imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
         else
-           imwrite(imind,cm,filename,'gif','DelayTime',0.3,'WriteMode','append'); 
+           imwrite(imind,cm,filename,'gif','DelayTime',0.3,...
+               'WriteMode','append'); 
         end
     end
     pause(0.3)
