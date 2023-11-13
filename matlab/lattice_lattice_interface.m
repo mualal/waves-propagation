5d977a134b1b8ea2d5f9fb85f1ca64db574fb6c2
mualal <almuravcev@yandex.ru>
2023-11-13 23:23:12 +0300

    add real-time monitor for total energy

--- matlab/lattice_lattice_interface.m
+++ matlab/lattice_lattice_interface.m
@@ -1,10 +1,18 @@
+% waves propagation
+
 %clc;
 clear;
 
-% time step to save disp
+% time step to save data
 save_time = 3;
-write_video = false;
+write_gif = false;
+gif_filename = '1.gif';
+gif_delay = 0.3;
+display_delay = 0.3;
+
+% data to display
 plot_var = 'energy';
+energy_monitor = true;
 
 % lattice geometry boundaries
 left_x = -100;
@@ -19,6 +27,12 @@ c = 0.75;
 omega = 1;
 a = 1;
 gamma = pi/4.5;
+
+% integration parameters
+dt = 0.01;
+t_max = 150;
+
+%--------------------------------------------------------------------------
 
 %k_1 = asin(omega/2 * sqrt(m_1/c)) * 2 / a;
 
@@ -105,8 +119,6 @@ for i=1:length(num_x)
 end
 
 
-dt = 0.01;
-t_max = 150;
 times = 0:dt:t_max;
 
 result_disp = cell(1, fix(t_max/save_time)+1);
@@ -146,6 +158,9 @@ for t=times
     %disp = disp + vel.*dt;
 end
 
+
+%--------------------------------------------------------------------------
+
 % set results to show
 result = result_disp;
 label = "перемещений";
@@ -181,6 +196,7 @@ view(17,22);
 hold off
 
 
+if ~strcmp(plot_var,'energy')||(strcmp(plot_var,'energy')&&~energy_monitor)
 f2=figure(2); hold on
 f2.Position = [50,50,750,650];
 s1 = surf(X,Y,result{end},'FaceAlpha',0.9,'EdgeAlpha',0.7);
@@ -203,32 +219,112 @@ for i = 1:length(result)
     l1.ZData = max(max(result{i}))*ones(1,length(x_coord));
     title('Цветовая карта '+label+' в момент времени t = '+...
         string((i-1)*save_time));
-    if write_video
-        filename = 'new-test-1.gif';
+    if write_gif
+        filename = gif_filename;
         frame = getframe(2); 
         im = frame2im(frame); 
         [imind,cm] = rgb2ind(im,256); 
         if i == 1
            imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
         else
-           imwrite(imind,cm,filename,'gif','DelayTime',0.3,...
+           imwrite(imind,cm,filename,'gif','DelayTime',gif_delay,...
                'WriteMode','append'); 
         end
     end
-    pause(0.3)
+    pause(display_delay)
 end
 hold off
+end
 
-
+if strcmp(plot_var,'energy') && ~energy_monitor
 f3=figure(3); hold on
 f3.Position = [50,50,750,650];
 plot(0:save_time:t_max,cell2mat(cellfun(@(x) sum(sum(x)),result_e,...
-    'UniformOutput',false)),'LineWidth',1,'Color','Blue')
-title('Зависимость полной энергии системы от времени');
+    'UniformOutput',false)),'LineWidth',1.7,'Color','Black')
+plot(0:save_time:t_max,cell2mat(cellfun(@(x) sum(sum(x(1:end,...
+    1:floor(end/2)))),result_e,'UniformOutput',false)),'LineWidth',1,...
+    'Color','Blue')
+plot(0:save_time:t_max,cell2mat(cellfun(@(x) sum(sum(x(1:end,...
+    ceil(end/2):end))),result_e,'UniformOutput',false)),'LineWidth',1,...
+    'Color','Red')
+title('Зависимость энергий в системе от времени');
 xlabel('Время, усл.ед.');
 ylabel('Энергия, усл.ед.');
+legend('Полная энергия системы', 'Энергия левой решётки',...
+    'Энергия правой решётки')
 grid on;
 grid minor;
+end
+
+
+if strcmp(plot_var,'energy') && energy_monitor
+e_sum = cell2mat(cellfun(@(x) sum(sum(x)),result_e,'UniformOutput',false));
+e_l = cell2mat(cellfun(@(x) sum(sum(x(1:end,1:floor(end/2)))),result_e,...
+    'UniformOutput',false));
+e_r = cell2mat(cellfun(@(x) sum(sum(x(1:end,ceil(end/2):end))),result_e,...
+    'UniformOutput',false));
+f2=figure(2); hold on
+f2.Position = [0,50,1400,650];
+til = tiledlayout(1,2,'TileSpacing','Compact');
+nexttile; hold on
+s1 = surf(X,Y,result{1},'FaceAlpha',0.9,'EdgeAlpha',0.7);
+title('Цветовая карта энергий');
+xlabel('Номер частицы по оси Ox');
+ylabel('Номер частицы по оси Oy');
+l1 = plot3(x_coord,y_coord,10*ones(1,length(x_coord)),'LineWidth',2,...
+    'Color','Black');
+colorbar;
+%caxis([-1 1])
+%caxis([-min(min(cell2mat(result))) max(max(cell2mat(result)))])
+%s1.EdgeColor = 'none';
+%view(17,22);
+axis([num_x(1) num_x(end) num_y(1) num_y(end)])
+pbaspect([1,(right_y-left_y)/(right_x-left_x),1]);
+hold off
+nexttile; hold on
+p1=plot(0,e_sum(1),'LineWidth',1.7,'Color','Black');
+p2=plot(0,e_l(1),'LineWidth',1,'Color','Blue');
+p3=plot(0,e_r(1),'LineWidth',1,'Color','Red');
+ylim([min([e_sum, e_l, e_r]) 1.1*max([e_sum, e_l, e_r])])
+title('Зависимость суммарных энергий в системе от времени');
+xlabel('Время, усл.ед.');
+ylabel('Энергия, усл.ед.');
+legend('Полная энергия системы', 'Энергия левой решётки',...
+    'Энергия правой решётки')
+grid on;
+grid minor;
+hold off
+til_t = title(til, 'Энергия в момент времени t=0');
+til_t.FontWeight = 'bold';
+for i = 1:length(result)
+    set(p1,'XData',(0:i-1)*save_time);
+    set(p1,'YData',e_sum(1:i));
+    set(p2,'XData',(0:i-1)*save_time);
+    set(p2,'YData',e_l(1:i));
+    set(p3,'XData',(0:i-1)*save_time);
+    set(p3,'YData',e_r(1:i));
+    drawnow;
+    s1.XData = X;
+    s1.YData = Y;
+    s1.ZData = result{i};
+    l1.ZData = max(max(result{i}))*ones(1,length(x_coord));
+    title(til, 'Энергия в момент времени t = '+string((i-1)*save_time));
+    if write_gif
+        filename = gif_filename;
+        frame = getframe(2); 
+        im = frame2im(frame); 
+        [imind,cm] = rgb2ind(im,256); 
+        if i == 1
+           imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
+        else
+           imwrite(imind,cm,filename,'gif','DelayTime',gif_delay,...
+               'WriteMode','append'); 
+        end
+    end
+    pause(display_delay)
+end
+hold off
+end
 
 
 function e = energy(m,c,vel,disp)
